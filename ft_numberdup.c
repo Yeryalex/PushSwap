3a5961d3e93c65a1bdc18dffbf62fb097114203e
Yery Alexander Rodriguez Gonzalez <yrodrigu@cbr4s6.42barcelona.com>
2024-06-20 20:24:00 +0200

    modified and final check for duplicated numbers

--- ft_numberdup.c
+++ ft_numberdup.c
@@ -6,30 +6,25 @@
 /*   By: yrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 18:48:13 by yrodrigu          #+#    #+#             */
-/*   Updated: 2024/06/20 19:22:09 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2024/06/20 20:22:38 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
               
 int ft_numberdup(t_list *lst)
 {
-    t_list *temp = lst;
-    t_list *compare;
+    t_list *temp;
 
-    if (!temp || !temp->next)
-        return 0;
-
-    while (temp != NULL && temp->next != NULL)
+    while (lst)
     {
-        compare = temp->next;
-        while (compare != NULL)
-        {
-            if (temp->number == compare->number)
-                return 1;
-
-            compare = compare->next;
-        }
-        temp = temp->next;
+	    temp = lst->next;
+	    while (temp)
+	    {
+		    if (lst->number == temp->number)
+			    return (1);
+	    	temp = temp->next;
+	    }
+    	lst = lst->next;
     }
-    return 0;
+    return (0);
 }

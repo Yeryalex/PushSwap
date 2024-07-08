abcfd00f4429916e826a87849e3039d9bfbe2c95
Yery Alexander Rodriguez Gonzalez <yeryalex03@gmail.com>
2024-07-08 21:00:03 +0200

    sorting till three in making

--- ft_sorting.c
+++ ft_sorting.c
@@ -6,11 +6,16 @@
 /*   By: yrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 19:08:44 by yrodrigu          #+#    #+#             */
-/*   Updated: 2024/07/08 20:17:52 by yrodrigu         ###   ########.fr       */
+/*   Updated: 2024/07/08 20:59:28 by yrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
-/*
+
+void	ft_stack_till_three()
+{
+
+}
+
 t_list	*ft_sort_b(t_list **stack)
 {
 	t_list	*stack_b;
@@ -27,17 +32,17 @@ t_list	*ft_sort_b(t_list **stack)
 		ft_sort_three(stack);
 	return (stack_b);
 }
-*/
+
 void	ft_sorting(t_list **stack)
 {
-/*	t_list	*stack_b;
+	t_list	*stack_b;
 
 	stack_b = NULL;
-*/
+
 	if (ft_lstsize(*stack) == 2)
 		ft_swap_a(stack);
-/*	else 
+	else 
 	{
 		stack_b = ft_sort_b(stack);
-	}*/
+	}
 }

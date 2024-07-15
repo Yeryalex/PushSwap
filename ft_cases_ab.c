ac37b20ed78249bcf48cbe7cdfeff9a800365f40
Yery Alexander Rodriguez Gonzalez <yeryalex03@gmail.com>
2024-07-15 19:34:13 +0200

    cases for rotation

---
+++ ft_cases_ab.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cases_ab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 19:31:45 by yrodrigu          #+#    #+#             */
+/*   Updated: 2024/07/15 19:33:52 by yrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "push_swap.h"
+
+int	ft_case_rarb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = ft_find_place_b(b, c);
+	if (i < ft_find_index(a, c))
+		i = ft_find_index(a, c);
+	return (i);
+}
+
+int	ft_case_rrarrb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_place_b(b, c))
+		i = ft_lstsize(b) - ft_find_place_b(b, c);
+	if ((i < (ft_lstsize(a) - ft_find_index(a, c))) && ft_find_index(a, c))
+		i = ft_lstsize(a) - ft_find_index(a, c);
+	return (i);
+}
+
+int	ft_case_rrarb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_index(a, c))
+		i = ft_lstsize(a) - ft_find_index(a, c);
+	i = ft_find_place_b(b, c) + i;
+	return (i);
+}
+
+int	ft_case_rarrb(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_place_b(b, c))
+		i = ft_lstsize(b) - ft_find_place_b(b, c);
+	i = ft_find_index(a, c) + i;
+	return (i);
+}
